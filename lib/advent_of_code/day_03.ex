910bd85337fad072f3d9a724dd27481a0f711512
Raymond Boswel <raymondboswel@gmail.com>
2022-12-06 11:02:54 +0200

    Day 3 part 1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,42 @@
 defmodule AdventOfCode.Day03 do
+  def parse_input do
+    input = AdventOfCode.Input.get!(3)
+
+    input
+    |> String.split("\n")
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.map(&String.graphemes/1)
+  end
+
   def part1(_args) do
+    input = parse_input()
+
+    sack = List.first(input)
+    {comp1, comp2} = Enum.split(sack, trunc(length(sack) / 2))
+
+    total =
+      Enum.map(input, fn sack ->
+        {comp1, comp2} = Enum.split(sack, trunc(length(sack) / 2))
+
+        Enum.filter(comp1, fn item -> Enum.any?(comp2, fn item2 -> item2 == item end) end)
+        |> Enum.dedup()
+        |> List.first()
+        |> letter_to_score()
+      end)
+      |> Enum.sum()
+
+    IO.inspect(total)
+  end
+
+  def letter_to_score(letter) do
+    is_upper = String.upcase(letter) == letter
+    ascii = letter |> String.to_charlist() |> hd
+
+    if(is_upper) do
+      ascii - 38
+    else
+      ascii - 96
+    end
   end
 
   def part2(_args) do
