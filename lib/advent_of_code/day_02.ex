89ccd927d66a00347fe44ffaa1d1e832c0dfd326
Raymond Boswel <raymondboswel@gmail.com>
2022-12-06 09:40:58 +0200

    Day 2 part 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -17,6 +17,81 @@ defmodule AdventOfCode.Day02 do
     IO.puts(total)
   end
 
+  def part2(_args) do
+    parsed_input = parse_input()
+
+    total =
+      Enum.map(parsed_input, fn round -> calc_score_2(round) end)
+      |> Enum.sum()
+
+    IO.puts(total)
+  end
+
+  def deduce_move_b(move_a, outcome) do
+    if outcome == :draw do
+      move_a
+    else
+      cond do
+        move_a == :rock ->
+          cond do
+            outcome == :b_loses ->
+              :scissors
+
+            outcome == :b_wins ->
+              :paper
+          end
+
+        move_a == :paper ->
+          cond do
+            outcome == :b_loses ->
+              :rock
+
+            outcome == :b_wins ->
+              :scissors
+          end
+
+        move_a == :scissors ->
+          cond do
+            outcome == :b_loses ->
+              :paper
+
+            outcome == :b_wins ->
+              :rock
+          end
+      end
+    end
+  end
+
+  def calc_score_2(round) do
+    [move_a, outcome] =
+      String.split(round, " ")
+      |> Enum.map(fn m ->
+        cond do
+          m == "A" ->
+            :rock
+
+          m == "B" ->
+            :paper
+
+          m == "C" ->
+            :scissors
+
+          m == "X" ->
+            :b_loses
+
+          m == "Y" ->
+            :draw
+
+          m == "Z" ->
+            :b_wins
+        end
+      end)
+
+    move_b = deduce_move_b(move_a, outcome)
+    calc_score(outcome, [move_a, move_b])
+  end
+
+  # Calculation for part1
   def calc_round(round) do
     player_moves =
       String.split(round, " ")
@@ -37,24 +112,27 @@ defmodule AdventOfCode.Day02 do
     calc_score(outcome, player_moves)
   end
 
+  def move_score(move) do
+    cond do
+      move == :rock -> 1
+      move == :paper -> 2
+      move == :scissors -> 3
+    end
+  end
+
+  def outcome_score(outcome) do
+    cond do
+      outcome == :b_loses -> 0
+      outcome == :draw -> 3
+      outcome == :b_wins -> 6
+    end
+  end
+
   def calc_score(outcome, moves) do
     [_, move_b] = moves
-
-    move_score =
-      cond do
-        move_b == :rock -> 1
-        move_b == :paper -> 2
-        move_b == :scissors -> 3
-      end
-
-    outcome_score =
-      cond do
-        outcome == :b_loses -> 0
-        outcome == :draw -> 3
-        outcome == :b_wins -> 6
-      end
-
-    move_score + outcome_score
+    move_total = move_score(move_b)
+    outcome_total = outcome_score(outcome)
+    move_total + outcome_total
   end
 
   def compare_moves(moves) do
@@ -83,8 +161,5 @@ defmodule AdventOfCode.Day02 do
           end
       end
     end
-  end
-
-  def part2(_args) do
   end
 end
