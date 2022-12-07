63a17bf5f3895cd8ab120a2e0b5ed45c9451c886
Raymond Boswel <raymondboswel@gmail.com>
2022-12-07 07:30:30 +0200

    Day 3 part 2

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -25,6 +25,32 @@ defmodule AdventOfCode.Day03 do
     IO.inspect(total)
   end
 
+  def part2(_args) do
+    input = parse_input()
+
+    elf_groups = Enum.chunk_every(input, 3)
+
+    total =
+      elf_groups
+      |> Enum.map(fn elf_group ->
+        [group1, group2, group3] = elf_group
+
+        Enum.filter(
+          group1,
+          fn item ->
+            Enum.any?(group2, fn item2 -> item2 == item end) &&
+              Enum.any?(group3, fn item3 -> item3 == item end)
+          end
+        )
+        # |> Enum.dedup()
+        |> List.first()
+        |> letter_to_score()
+      end)
+      |> Enum.sum()
+
+    IO.inspect(total)
+  end
+
   def letter_to_score(letter) do
     is_upper = String.upcase(letter) == letter
     ascii = letter |> String.to_charlist() |> hd
@@ -34,8 +60,5 @@ defmodule AdventOfCode.Day03 do
     else
       ascii - 96
     end
-  end
-
-  def part2(_args) do
   end
 end
